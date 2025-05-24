fad0e1843eea4e6f4773a3dbce39d429b4b1501a
Auri Gabriel <ririmello.a@gmail.com>
2025-05-24 15:07:41 -0300

    feat: add error handling for grocery item loading

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -17,6 +17,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -30,6 +31,13 @@ class _GroceryListState extends State<GroceryList> {
       'shopping-list.json',
     );
     final response = await http.get(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = "Failed to fetch data. Please try again later.";
+      });
+      return;
+    }
 
     final Map<String, dynamic> listData = json.decode(response.body);
 
@@ -103,6 +111,10 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );
+    }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
